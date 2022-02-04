da0b3871b89a69e8f132ac617b40a4952603ed29
Artem Frolov <Artem.Frolov@kaspersky.com>
2022-02-04 18:22:44 +0300

    fix 10

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -5,7 +5,7 @@ from django.test import Client, TestCase
 class StaticPagesURLTests(TestCase):
     def setUp(self):
         self.guest_client = Client()
-
+        
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
         response = self.guest_client.get("/")
