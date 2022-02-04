ef92441f25a23c8b372c8a4ae8018356a0d74f38
Artem Frolov <Artem.Frolov@kaspersky.com>
2022-02-04 17:09:50 +0300

    fix

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse.status_code('У меня получилось!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse.status_code('А это вторая страница')
+    return HttpResponse('А это вторая страница')
