639031f10c3fad38fa2da315d951228d1ae39a83
Artem Frolov <Artem.Frolov@kaspersky.com>
2022-02-04 17:39:35 +0300

    fix 5

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 from django.test import Client, TestCase
+from django.core.cache import cache
 
 
 class StaticPagesURLTests(TestCase):
@@ -8,16 +9,20 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
+        cache.clear()
         response = self.guest_client.get("/")
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
-
+        
+        cache.clear()
         response = self.guest_client.get("/second_page/")
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
+        cache.clear()
         response = self.guest_client.get("/")
-        self.assertContains(response.context, "У меня получилось!")
-
+        self.assertContains(response, "У меня получилось!")
+        
+        cache.clear()
         response = self.guest_client.get("/second_page/")
-        self.assertContains(response.context, "А это вторая страница!")
+        self.assertContains(response, "А это вторая страница!")
